c691667937ab5d6fecbfc8ff936dc464983a04d1
Sergei <Faoxis@mail.ru>
2016-05-17 08:46:00 +0300

    week 1 is complete

--- Demo.py
+++ Demo.py
@@ -1,1 +1,15 @@
-print('hello' if 3 > 2 else 'bye')
+import time
+
+class Loggable:
+    def log(self, msg):
+        print(str(time.ctime()) + ": " + str(msg))
+
+class LoggableList(list, Loggable):
+    def append(self, obj):
+        super(LoggableList, self).append(obj)
+        super(LoggableList, self).log(obj)
+
+
+lst = LoggableList()
+lst.append('something1')
+lst.append('something2')
